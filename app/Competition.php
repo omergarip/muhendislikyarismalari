0bcaa67fbf042ebd051fd2058f9cfca45e8ac0b1
Omer Garip <omergarip95@gmail.com>
2019-09-29 14:56:54 -0400

    Relation between user and competition table has been established

--- app/Competition.php
+++ app/Competition.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Competition extends Model
 {
     use SoftDeletes;
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
